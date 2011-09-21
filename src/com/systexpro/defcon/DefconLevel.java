b97257950b669777d74f8fd7314969bd9d3ba04a
Christian Horton <christian@tweakuniverse.com>
2011-09-21 00:23:34 -0700

    Better Level System

---
+++ src/com/systexpro/defcon/DefconLevel.java
@@ -0,0 +1,24 @@
+package com.systexpro.defcon;
+
+public enum DefconLevel {
+
+	LEVEL_0(0),
+	LEVEL_1(1),
+	LEVEL_2(2),
+	LEVEL_3(3),
+	LEVEL_4(4),
+	LEVEL_5(5),
+	LEVEL_6(6);
+	
+	int level;
+	
+	DefconLevel(int i) {
+		level = i;
+	}
+	
+	public int getLevel() {
+		return level;
+	}
+	
+	
+}
